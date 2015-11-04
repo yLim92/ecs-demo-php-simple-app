3cbe8b35cddadf70bb5b52c8fc0c1503b7041b0e
yLim92 <yon_gee@yahoo.com>
2015-11-04 14:39:53 -0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -17,7 +17,7 @@
             <div class="hero-unit">
                 <h1>Hell f-ing yeah! It's the BEST damn PHP App EVER!</h1>
                 <h2>Congratulations</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS. It's also the BEST!</p>
+                <p>Your PHP application is now running on a container in Amazon ECS. Isn't that crazy? It's also the BEST!</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
